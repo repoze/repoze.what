0c862ab7c9cc887c15e80d244b6b5a52e9014fc2
Gustavo Narea <me@gustavonarea.net>
2008-11-22 18:43:35 +0000

    Corrected the version/revision numbers in the Sphinx based docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -42,9 +42,9 @@ copyright = u'2008, The Repoze Project'
 # other places throughout the built documents.
 #
 # The short X.Y version.
-version = '0.1'
+version = '0.9'
 # The full version, including alpha/beta/rc tags.
-release = '0.1a2'
+release = '0.9a2'
 
 # There are two options for replacing |today|: either, you set today to some
 # non-false value, then it is used:
