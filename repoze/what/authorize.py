88d887c6bd9f60c62a72793051acb9e05c7b417f
Gustavo Narea <me@gustavonarea.net>
2009-01-19 16:41:19 +0000

    Minor change, avoiding type casting

--- repoze/what/authorize.py
+++ repoze/what/authorize.py
@@ -41,6 +41,6 @@ def check_authorization(predicate, environ):
     logger = environ.get('repoze.who.logger')
     if predicate and not predicate.eval_with_environ(environ):
         exc = NotAuthorizedError(predicate.error)
-        logger and logger.info('Authorization denied: %s' % unicode(exc))
+        logger and logger.info('Authorization denied: %s' % predicate.error)
         raise exc
     logger and logger.info('Authorization granted')
