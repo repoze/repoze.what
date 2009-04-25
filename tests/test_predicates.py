9182c6695a930df91a57538e265e8c53e87c5eb1
Gustavo Narea <me@gustavonarea.net>
2009-04-25 17:47:16 +0000

    Removed the user-related predicate checkers.

--- tests/test_predicates.py
+++ tests/test_predicates.py
@@ -23,7 +23,6 @@ from unittest import TestCase
 from repoze.what.exc import NotAuthorizedError
 from repoze.what.predicates.base import Predicate, CompoundPredicate
 from repoze.what.predicates.generic import All, Any, Not
-from repoze.what.predicates.user import is_user, is_anonymous, not_anonymous
 
 from tests.base import make_environ, FakeLogger
 
@@ -232,48 +231,6 @@ class TestAnyPredicate(BasePredicateTester):
         environ = make_environ(test_number=5)
         p = Any(EqualsFour(), GreaterThan(3))
         self.eval_met_predicate(p, environ)
-
-
-class TestIsUserPredicate(BasePredicateTester):
-    
-    def test_right_user(self):
-        environ = make_environ('gustavo')
-        p = is_user('gustavo')
-        self.eval_met_predicate(p, environ)
-    
-    def test_wrong_user(self):
-        environ = make_environ('andreina')
-        p = is_user('gustavo')
-        self.eval_unmet_predicate(p, environ,
-                                  'The current user must be "gustavo"')
-
-
-class TestIsAnonymousPredicate(BasePredicateTester):
-    
-    def test_authenticated_user(self):
-        environ = make_environ('gustavo')
-        p = is_anonymous()
-        self.eval_unmet_predicate(p, environ,
-                                  'The current user must be anonymous')
-    
-    def test_anonymous_user(self):
-        environ = make_environ()
-        p = is_anonymous()
-        self.eval_met_predicate(p, environ)
-
-
-class TestNotAnonymousPredicate(BasePredicateTester):
-    
-    def test_authenticated_user(self):
-        environ = make_environ('gustavo')
-        p = not_anonymous()
-        self.eval_met_predicate(p, environ)
-    
-    def test_anonymous_user(self):
-        environ = make_environ()
-        p = not_anonymous()
-        self.eval_unmet_predicate(p, environ,
-                         'The current user must have been authenticated')
 
 
 #{ Mock definitions
