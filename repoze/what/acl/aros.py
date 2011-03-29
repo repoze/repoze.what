fcedcaf08f068c83cdc4637b968feffb6f937421
Gustavo Narea <me@gustavonarea.net>
2011-03-29 22:02:50 +0100

    Implemented Access Request Objects

---
+++ repoze/what/acl/aros.py
@@ -0,0 +1,49 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+# Copyright (c) 2011, Gustavo Narea <me@gustavonarea.net>.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the BSD-like license at
+# http://www.repoze.org/LICENSE.txt.  A copy of the license should accompany
+# this distribution.  THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL
+# EXPRESS OR IMPLIED WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO,
+# THE IMPLIED WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND
+# FITNESS FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+"""
+Built-in Access Request Objects.
+
+A repoze.what ARO is a callable that takes the request object and returns
+a boolean to report whether the requester matches the one it represents.
+
+TODO: Implement ARO for groups once group adapters have been implemented
+
+"""
+
+
+__all__ = ["ANONYMOUS", "AUTHENTICATED", "User"]
+
+
+AUTHENTICATED = lambda request: bool(request.remote_user)
+"""ARO that matches any authenticated user."""
+
+
+ANONYMOUS = lambda request: not AUTHENTICATED(request)
+"""ARO that matches anonymous users."""
+
+
+class User(object):
+    """Access Request Object that matches a specific user."""
+    
+    def __init__(self, user_id):
+        """
+        
+        :param user_id: The Id of the expected user.
+        
+        """
+        self.user_id = user_id
+    
+    def __call__(self, request):
+        return request.remote_user == self.user_id
---
+++ tests/acl/test_aros.py
@@ -0,0 +1,73 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+# Copyright (c) 2011, Gustavo Narea <me@gustavonarea.net>.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the BSD-like license at
+# http://www.repoze.org/LICENSE.txt.  A copy of the license should accompany
+# this distribution.  THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL
+# EXPRESS OR IMPLIED WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO,
+# THE IMPLIED WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND
+# FITNESS FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+"""
+Tests for the built-in Access Request Objects.
+
+"""
+
+from nose.tools import assert_false, ok_
+from webob import Request
+
+from repoze.what.acl.aros import *
+
+
+class TestAnonymous(object):
+    """Tests for the ANONYMOUS ARO."""
+    
+    def test_anonymous(self):
+        """There's a match if and only if there's no REMOTE_USER"""
+        request = Request.blank("/")
+        ok_(ANONYMOUS(request))
+    
+    def test_authenticated(self):
+        """There's no match in authenticated requests."""
+        request = Request.blank("/", {'REMOTE_USER': "andreina"})
+        assert_false(ANONYMOUS(request))
+
+
+class TestAuthenticated(object):
+    """Tests for the AUTHENTICATED ARO."""
+    
+    def test_anonymous(self):
+        """There's no match in authenticated requests."""
+        request = Request.blank("/")
+        assert_false(AUTHENTICATED(request))
+    
+    def test_authenticated(self):
+        """There's a match if and only if there's no REMOTE_USER"""
+        request = Request.blank("/", {'REMOTE_USER': "andreina"})
+        ok_(AUTHENTICATED(request))
+
+
+class TestUser(object):
+    """Tests for the :class:`User` ARO."""
+    
+    def test_anonymous(self):
+        """There's no match in anonymous requests."""
+        aro = User("carla")
+        request = Request.blank("/")
+        assert_false(aro(request))
+    
+    def test_authenticated_but_not_right_user(self):
+        """There's no match in authenticated requests from a different user."""
+        aro = User("carla")
+        request = Request.blank("/", {'REMOTE_USER': "andreina"})
+        assert_false(aro(request))
+    
+    def test_authenticated_and_right_user(self):
+        """There's a match if and only if the expected user made the request."""
+        aro = User("carla")
+        request = Request.blank("/", {'REMOTE_USER': "carla"})
+        ok_(aro(request))
