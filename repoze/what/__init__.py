121c51b7660feb201d275021d1018dc16e99d17b
Gustavo Narea <me@gustavonarea.net>
2011-04-13 22:45:31 +0100

    repoze.what.plugins is gone. It will not even be a namespace package -- Plugins for v2 will have to use a different namespace

--- repoze/what/__init__.py
+++ repoze/what/__init__.py
@@ -1,9 +1,7 @@
 # -*- coding: utf-8 -*-
 ##############################################################################
 #
-# Copyright (c) 2007, Agendaless Consulting and Contributors.
-# Copyright (c) 2008, Florent Aide <florent.aide@gmail.com>.
-# Copyright (c) 2008-2010, Gustavo Narea <me@gustavonarea.net>.
+# Copyright (c) 2011, Gustavo Narea <me@gustavonarea.net>.
 # All Rights Reserved.
 #
 # This software is subject to the provisions of the BSD-like license at
@@ -14,14 +12,4 @@
 # FITNESS FOR A PARTICULAR PURPOSE.
 #
 ##############################################################################
-
-# This is a package. Do NOT put anything here to keep the repoze.what
-# namespace clear for plugins at repoze.what.plugins
-
-# See http://peak.telecommunity.com/DevCenter/setuptools#namespace-packages
-
-try: #pragma: no cover
-    __import__('pkg_resources').declare_namespace(__name__)
-except ImportError: #pragma: no cover
-    from pkgutil import extend_path
-    __path__ = extend_path(__path__, __name__)
+"""repoze.what"""
